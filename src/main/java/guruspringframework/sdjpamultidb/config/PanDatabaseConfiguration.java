b9f95b776e8e10c970c9d1bbdf8a81e5ea4cdfca
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-03-13 08:48:24 -0300

    create entity managers

--- src/main/java/guruspringframework/sdjpamultidb/config/PanDatabaseConfiguration.java
+++ src/main/java/guruspringframework/sdjpamultidb/config/PanDatabaseConfiguration.java
@@ -1,12 +1,15 @@
 package guruspringframework.sdjpamultidb.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import guruspringframework.sdjpamultidb.domain.creditcard.CreditCard;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import javax.sql.DataSource;
 
@@ -21,9 +24,19 @@ public class PanDatabaseConfiguration {
 
     @Primary
     @Bean
-    public DataSource cardPanHolderDataSource(@Qualifier("cardPanDataSourceProperties") DataSourceProperties cardPanDataSourceProperties) {
+    public DataSource cardPanDataSource(@Qualifier("cardPanDataSourceProperties") DataSourceProperties cardPanDataSourceProperties) {
         return this.cardPanDataSourceProperties().initializeDataSourceBuilder()
                 .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean cardPanEntityManagerFactory(
+            @Qualifier("cardPanDataSource") DataSource cardPanDatasource,
+            EntityManagerFactoryBuilder builder) {
+        return builder.dataSource(cardPanDatasource)
+                .packages(CreditCard.class)
+                .persistenceUnit("pan")
                 .build();
     }
 }
