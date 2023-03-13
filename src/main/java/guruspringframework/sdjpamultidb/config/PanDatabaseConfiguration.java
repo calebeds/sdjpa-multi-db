192aaf42a95f8016b8a0684de8c4e78388cd423a
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-03-13 08:22:12 -0300

    add initial datasource configurations

--- src/main/java/guruspringframework/sdjpamultidb/config/PanDatabaseConfiguration.java
+++ src/main/java/guruspringframework/sdjpamultidb/config/PanDatabaseConfiguration.java
@@ -1,17 +1,29 @@
 package guruspringframework.sdjpamultidb.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class PanDatabaseConfiguration {
     @Bean
     @Primary
     @ConfigurationProperties("spring.pan.datasource")
-    public DataSourceProperties cardHolderDataSourceProperties() {
+    public DataSourceProperties cardPanDataSourceProperties() {
         return new DataSourceProperties();
+    }
+
+    @Primary
+    @Bean
+    public DataSource cardPanHolderDataSource(@Qualifier("cardPanDataSourceProperties") DataSourceProperties cardPanDataSourceProperties) {
+        return this.cardPanDataSourceProperties().initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .build();
     }
 }
