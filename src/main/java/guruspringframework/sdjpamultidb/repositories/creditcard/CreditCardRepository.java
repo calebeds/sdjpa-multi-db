43a7a1f1bd73fbb74c021615129e24890d5bd1b6
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-03-21 09:54:04 -0300

    create getCreditCarcById service method

--- src/main/java/guruspringframework/sdjpamultidb/repositories/creditcard/CreditCardRepository.java
+++ src/main/java/guruspringframework/sdjpamultidb/repositories/creditcard/CreditCardRepository.java
@@ -3,6 +3,8 @@ package guruspringframework.sdjpamultidb.repositories.creditcard;
 import guruspringframework.sdjpamultidb.domain.creditcard.CreditCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface CreditCardRepository extends JpaRepository<CreditCard, Long> {
 }
--- src/main/java/guruspringframework/sdjpamultidb/repositories/pan/CreditCardPANRepository.java
+++ src/main/java/guruspringframework/sdjpamultidb/repositories/pan/CreditCardPANRepository.java
@@ -3,5 +3,9 @@ package guruspringframework.sdjpamultidb.repositories.pan;
 import guruspringframework.sdjpamultidb.domain.pan.CreditCardPAN;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public interface CreditCardPANRepository extends JpaRepository<CreditCardPAN, Long> {
+    Optional<CreditCardPAN> findByCreditCardId(Long id);
 }
